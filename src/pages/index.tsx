b1814940a85d718e2263b6dcd1ae68022207d96c
Alan Neres <alanneres10@hotmail.com>
2021-05-12 00:05:12 -0300

    chore: add mapbox for style map

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,23 @@
-import dynamic from 'next/dynamic'
+import HomeTemplate from 'templates/Home'
+import { MapProps } from 'components/Map'
+import { GetStaticProps } from 'next'
+import client from 'graphql/client'
+import { GetPlacesQuery } from 'graphql/generated/graphql'
+import { GET_PLACES } from 'graphql/queries'
 
-const Map = dynamic(() => import('components/Map'), { ssr: false })
+export default function Home({ places }: MapProps) {
+  return <HomeTemplate places={places} />
+}
 
-export default function Home() {
-  return <Map />
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { places } = await client.request<GetPlacesQuery>(GET_PLACES)
+
+  if (!places) return { notFound: true }
+
+  return {
+    props: {
+      places
+    }
+  }
 }
